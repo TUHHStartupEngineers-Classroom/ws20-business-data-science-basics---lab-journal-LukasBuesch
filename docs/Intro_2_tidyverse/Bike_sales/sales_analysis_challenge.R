1ebac5bce68a108048e43263b3e660f5b3f0143a
LukasBuesch <55090980+LukasBuesch@users.noreply.github.com>
2020-11-28 21:30:51 +0100

    changes

--- docs/Intro_2_tidyverse/Bike_sales/sales_analysis_challenge.R
+++ docs/Intro_2_tidyverse/Bike_sales/sales_analysis_challenge.R
@@ -8,15 +8,13 @@ library(lubridate)
 
 
 # 2.0 Importing Files ----
-# A good convention is to use the file name and suffix it with tbl for the data structure tibble
+
 bikes_tbl      <- read_excel(path = "DS_101/00_data/01_bike_sales/01_raw_data/bikes.xlsx")
 orderlines_tbl <- read_excel("DS_101/00_data/01_bike_sales/01_raw_data/orderlines.xlsx")
 bikeshops_tbl  <- read_excel("DS_101/00_data/01_bike_sales/01_raw_data/bikeshops.xlsx")
 
-# 3.0 Examining Data ----
 
-
-# 4.0 Joining Data ----
+# 3.0 Joining Data ----
 
 # leftjoin the specified columns to the orderlines_tbl
 
@@ -30,57 +28,61 @@ bike_orderlines_joined_tbl <- orderlines_tbl %>%
 # Split the location column
 
 bike_orderlines_wrangled_tbl <- bike_orderlines_joined_tbl %>%
+  
   # 5.1 Separate location name
+  
   separate(col    = location,
            into   = c("city", "state"),
            sep    = ", ") %>%
   
   # 5.2 Add the total price (price * quantity) 
   # Add a column to a tibble that uses a formula-style calculation of other columns
+  
   mutate(total.price = price * quantity) %>%
   
   # 5.3 Optional: Reorganize. Using select to grab or remove unnecessary columns
   # 5.3.1 by exact column name
+  
   select(-...1, -gender) %>%
   
   # 5.3.2 by a pattern
-  # You can use the select_helpers to define patterns. 
-  # Type ?ends_with and click on Select helpers in the documentation
+  
   select(-ends_with(".id")) %>%
   
   # 5.3.3 Actually we need the column "order.id". Let's bind it back to the data
   bind_cols(bike_orderlines_joined_tbl %>% select(order.id)) %>% 
   
   # 5.3.4 You can reorder the data by selecting the columns in your desired order.
-  # You can use select_helpers like contains() or everything()
+  
   select(order.id, contains("order"), contains("model"), contains("category"),
          price, quantity, total.price,
          everything()) %>%
   
-  # 5.4 Rename columns because we actually wanted underscores instead of the dots
-  # (one at the time vs. multiple at once)
+  # 5.4 Rename columns -> dots to underscores
+  
   rename(bikeshop = name) %>%
   set_names(names(.) %>% str_replace_all("\\.", "_"))
 
 # 6.0 Business Insights ----
-# 6.1 Sales by Year ----
+# 6.1 Sales by location (state) ----
 
 # Step 1 - Manipulate
+
 sales_by_location_tbl <- bike_orderlines_wrangled_tbl %>%
   
   # Select columns
+  
   select(state, total_price) %>%
   
-  # Add year column
-  # mutate(year = year(order_date)) %>%
+  # Grouping by location and summarizing sales
   
-  # Grouping by year and summarizing sales
   group_by(state) %>% 
   summarize(sales = sum(total_price)) %>%
   
   # Optional: Add a column that turns the numbers into a currency format 
   # (makes it in the plot optically more appealing)
   # mutate(sales_text = scales::dollar(sales)) <- Works for dollar values
+  
   mutate(sales_text = scales::dollar(sales, big.mark = ".", 
                                      decimal.mark = ",", 
                                      prefix = "", 
@@ -90,10 +92,12 @@ sales_by_location_tbl <- bike_orderlines_wrangled_tbl %>%
 
 sales_by_location_tbl %>%
   
-  # Setup canvas with the columns year (x-axis) and sales (y-axis)
+  # Setup canvas with the columns state (x-axis) and sales (y-axis)
+  
   ggplot(aes(x = state, y = sales)) +
   
   # Geometries
+  
   geom_col(fill = "#2DC6D6") + # Use geom_col for a bar plot
   geom_label(aes(label = sales_text)) + # Adding labels to the bars
   geom_smooth(method = "lm", se = FALSE) + # Adding a trendline
@@ -101,14 +105,14 @@ sales_by_location_tbl %>%
   
   # Formatting
   # scale_y_continuous(labels = scales::dollar) + # Change the y-axis. 
-  # Again, we have to adjust it for euro values
+  
   scale_y_continuous(labels = scales::dollar_format(big.mark = ".", 
                                                     decimal.mark = ",", 
                                                     prefix = "", 
                                                     suffix = " €")) +
   labs(
-    title    = "Revenue by year",
-    subtitle = "Upward Trend",
+    title    = "Revenue by state",
+    subtitle = "-",
     x = "", # Override defaults for x and y
     y = "Revenue"
   )
@@ -120,10 +124,12 @@ sales_by_location_tbl %>%
 sales_by_location_year_tbl <- bike_orderlines_wrangled_tbl %>%
   
   # Select columns and add a year
+  
   select(order_date, total_price, state) %>%
   mutate(year = year(order_date)) %>%
   
-  # Group by and summarize year and main catgegory
+  # Group by and summarize year and state
+  
   group_by(year, state) %>%
   summarise(sales = sum(total_price)) %>%
   ungroup() %>%
@@ -154,8 +160,8 @@ sales_by_location_year_tbl %>%
                                                     prefix = "", 
                                                     suffix = " €")) +
   labs(
-    title = "Revenue by year and main category",
-    subtitle = "Each product category has an upward trend",
-    fill = "Main category" # Changes the legend name
+    title = "Revenue by year and state",
+    subtitle = "-",
+    fill = "states" # Changes the legend name
   )
 
