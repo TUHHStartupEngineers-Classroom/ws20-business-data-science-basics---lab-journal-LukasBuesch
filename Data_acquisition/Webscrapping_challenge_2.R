480bf4e9e2ad99fede8c9e6db4db316583cb0bbb
LukasBuesch <55090980+LukasBuesch@users.noreply.github.com>
2020-11-29 19:05:12 +0100

    saves

--- Data_acquisition/Webscrapping_challenge_2.R
+++ Data_acquisition/Webscrapping_challenge_2.R
@@ -4,16 +4,15 @@
 
 library(tidyverse) # Main Package - Loads dplyr, purrr, etc.
 library(rvest)     # HTML Hacking & Web Scraping
-library(xopen)     # Quickly opening URLs
 library(jsonlite)  # converts JSON files to R objects
 library(glue)      # concatenate strings
 library(stringi)   # character string/text processing
+library(purrr)
 
 
 # 1.1 COLLECT PRODUCT FAMILIES ----
 
 url_home          <- "https://www.rosebikes.de/"
-xopen(url_home) # Open links directly from RStudio to inspect them
 
 # Read in the HTML for the entire webpage
 html_home         <- read_html(url_home)
@@ -82,8 +81,6 @@ bike_category_url_tbl <- bike_family_tbl %>%
 # select first bike category url
 bike_category_url <- bike_category_url_tbl$url[1]
 
-xopen(bike_category_url)
-
 # Get the URLs for the bikes of the first category
 html_bike_category  <- read_html(bike_category_url)
 bike_sub_category_url_tbl        <- html_bike_category %>%
@@ -109,8 +106,6 @@ bike_sub_category_url_tbl        <- html_bike_category %>%
 
 bike_sub_category_url <- bike_sub_category_url_tbl$url[1]
 
-xopen(bike_sub_category_url)
-
 # Get the URLs for the bikes of the first subcategory
 
 html_bike_sub_category  <- read_html(bike_sub_category_url)
@@ -130,292 +125,72 @@ bike_url_tbl        <- html_bike_sub_category %>%
 
 
   # Add the full URL
-
-  mutate(url = glue("https://www.rosebikes.de{url}")) %>%
   
-  # delete multiple entries
+bike_url_tbl$url <- paste("https://www.rosebikes.de", trimws(bike_url_tbl$url), sep = "")
+
+
+
   
-  distinct(url)
-
-
-
-
+  
 # 2.1.2 Extract the descriptions (since we have retrieved the data already)
-bike_desc_tbl <- html_bike_category %>%
+bike_url <- bike_url_tbl$url[1]
+html_bike <- read_html(bike_url)
+
+bike_desc_tbl <- html_bike %>%
   
   # Get the nodes in the meta tag where the attribute itemprop equals description
-  html_nodes('.productTile__productSummaryLeft > meta[itemprop="description"]') %>%
+  html_nodes(".buybox__prize__wrapper > span") %>%
   
   # Extract the content of the attribute content
-  html_attr("content") %>%
+  html_attr("data-test") %>%
   
   # Convert vector to tibble
-  enframe(name = "position", value = "description")
-
-
-
-# 2.1.3 Get even more data from JSON files
-bike_json_tbl  <- html_bike_category %>%
-  
-  html_nodes(css = '.productGrid__listItem.xlt-producttile > div') %>%
-  html_attr("data-gtm-impression") %>%
-  
-  # Convert the JSON format to dataframe
-  # map runs that function on each element of the list
-  map(fromJSON) %>% # need JSON ### need lists
-  
-  # Extract relevant information of the nested list
-  map(purrr::pluck, 2, "impressions") %>% # Need purrr and expl above
-  
-  # Set "not defined" and emtpy fields to NA (will be easier to work with)
-  map(na_if, "not defined") %>%
-  map(na_if, "") %>%
-  
-  # The class of dimension56 and price varies between numeric and char.
-  # This converts this column in each list to numeric
-  # across allows to perform the same operation on multiple columns
-  map(~mutate(., across(c("dimension56","price"), as.numeric))) %>%
-  
-  # Stack all lists together
-  bind_rows() %>%
-  # Convert to tibble so that we have the same data format
-  as_tibble() %>%
-  
-  # Add consecutive numbers so that we can bind all data together
-  # You could have also just use bind_cols()
-  rowid_to_column(var='position') %>%
-  left_join(bike_desc_tbl) %>%
-  left_join(bike_url_tbl)
+  enframe(name = "position", value = "price")
 
 
 
 
-get_bike_data <- function(url) {
-  
-  html_bike_category <- read_html(url)
-  
-  # Get the URLs
-  bike_url_tbl  <- html_bike_category %>%
-    html_nodes(css = ".productTile__contentWrapper > a") %>%
-    html_attr("href") %>%
-    str_remove(pattern = "\\?.*") %>%
-    enframe(name = "position", value = "url")
+
+get_bike_data <- function(bike_url) {
   
   # Get the descriptions
-  bike_desc_tbl <- html_bike_category %>%
-    html_nodes(css = '.productTile__productSummaryLeft > 
-                      meta[itemprop="description"]') %>%
-    html_attr("content") %>%
-    enframe(name = "position", value = "description")
+  html_bike <- read_html(bike_url)
+  bike_desc_tbl <- html_bike %>%
+    html_nodes(".buybox__prize__wrapper > span") %>%
+    html_attr("data-test") %>%
+    enframe(name = "position", value = "price")
   
-  # Get JSON data
-  bike_json_tbl <- html_bike_category %>%
-    html_nodes(css = '.productGrid__listItem.xlt-producttile > div') %>%
-    html_attr("data-gtm-impression") %>%
-    map(fromJSON) %>% # need JSON ### need lists
-    map(purrr::pluck, 2, "impressions") %>% 
-    map(na_if, "not defined") %>%
-    map(na_if, "") %>%
-    map(~mutate(., across(c("dimension56","price"), as.numeric))) %>%
-    bind_rows() %>%
-    as_tibble() %>%
-    rowid_to_column(var='position') %>%
-    left_join(bike_desc_tbl) %>%
-    left_join(bike_url_tbl)
 }
 
+
+
+
+
 # Run the function with the first url to check if it is working
-bike_category_url <- bike_category_tbl$url[1]
-bike_data_tbl     <- get_bike_data(url = bike_category_url)
+bike_url <- bike_url_tbl$url[1]
+bike_data_tbl <- get_bike_data(bike_url)
 
 bike_data_tbl
+
 
 
 # 2.3.1a Map the function against all urls
 
 # Extract the urls as a character vector
-bike_category_url_vec <- bike_category_tbl %>% 
+bike_url_vec <- bike_url_tbl %>% 
   pull(url)
 
 # Run the function with every url as an argument
-bike_data_lst <- map(bike_category_url_vec, get_bike_data)
+bike_data_lst <- map(bike_url_vec, get_bike_data)
 
 # Merge the list into a tibble
 bike_data_tbl <- bind_rows(bike_data_lst)
-saveRDS(bike_data_tbl, "Data_acquisition/bike_data_tbl.rds")
 
 
-# Check for duplicates
-bike_data_tbl %>%
-  group_by(id) %>%
-  filter(n()>1) %>%
-  arrange(id) %>% 
-  View()
+name_with_price = bind_cols(bike_data_tbl,bike_url_tbl$url)
 
-
-# Filter non Canyon bikes (based on id length) and add an empty column for the colors
-bike_data_cleaned_tbl <- bike_data_tbl %>%
-  
-  # Filter for bikes. Only unique ones
-  filter(nchar(.$id) == 4) %>%
-  filter(!(name %>% str_detect("Frameset"))) %>%
-  distinct(id, .keep_all = T) %>%
-  
-  # Split categories (Speedmax had to be treated individually)
-  mutate(category = replace(category, 
-                            name == "Speedmax CF SLX 8.0 SL", "Road/Triathlon Bike/Speedmax")) %>%
-  separate(col = category, into = c("category_1",
-                                    "category_2",
-                                    "category_3"),
-           sep = "(?<!\\s)/(?!\\s)") %>%
-  
-  # Renaming
-  rename("year"       = "dimension50") %>%
-  rename("model"      = "name") %>%
-  rename("gender"     = "dimension63") %>%
-  rename("price_euro" = "metric4") %>%
-  
-  # Fix years manually (have checked the website)
-  mutate(year = replace_na(year, 2021)) %>%
-  
-  # Add frame material
-  mutate(frame_material = case_when(
-    model %>% str_detect(" CF ") ~ "carbon",
-    model %>% str_detect(" CFR ") ~ "carbon",
-    TRUE ~ "aluminium"
-  )
-  ) %>%
-  
-  # Select and order columns
-  select(-c(position, brand, variant, starts_with("dim"), 
-            quantity, feedProductId, price, metric5)) %>%
-  select(id, model, year, frame_material, price_euro, everything())
-
-saveRDS(bike_data_cleaned_tbl, "Data_acquisition/bike_data_cleaned_tbl.rds")
+name_with_price
 
 
 
-
-# 3.1a Get all color variations for each bike
-
-# Extract all bike urls
-bike_url_vec <- bike_data_cleaned_tbl %>% 
-  pull(url)
-
-# Create function to get the variations
-get_colors <- function(url) {
-  
-  url %>%
-    
-    read_html() %>%
-    
-    # Get all 'script nodes' and convert to char
-    html_nodes(css = "script") %>%
-    as.character() %>%
-    
-    # Select the node, that contains 'window.deptsfra'
-    str_subset(pattern = "window.deptsfra") %>%
-    
-    # remove the chars that do not belong to the json
-    # 1. replace at the beginning everything until the first "{" with ""
-    str_replace("^[^\\{]+", "") %>%
-    # 2. replace at the end everything after the last "}" with ""
-    str_replace("[^\\}]+$", "") %>%
-    
-    # Convert from json to an r object and pick the relevant values
-    fromJSON() %>%
-    purrr::pluck("productDetail", "variationAttributes", "values", 1, "value")
-}
-
-# Run the function over all urls and add result to bike_data_cleaned_tbl
-# This will take a long time (~ 20-30 minutes) because we have to iterate over many bikes
-bike_data_colors_tbl <- bike_data_cleaned_tbl %>% 
-  mutate(colors = map(bike_url_vec, get_colors))
-
-saveRDS(bike_data_colors_tbl, "Data_acquisition/bike_data_colors_tbl.rds")
-
-
-library(furrr)     # Parallel Processing using purrr (iteration)
-plan("multiprocess")
-bike_data_colors_tbl <- bike_data_cleaned_tbl %>% 
-  mutate(colors = future_map(bike_url_vec, get_colors))
-
-
-
-# 3.2 Create the urls for each variation
-
-bike_data_colors_tbl <- bike_data_colors_tbl %>%
-  
-  # Create entry for each color variation
-  unnest(colors) %>%
-  
-  # Merge url and query parameters for the colors
-  mutate(url_color = glue("{url}?dwvar_{id}_pv_rahmenfarbe={colors}")) %>%
-  select(-url) %>%
-  
-  # Use stringi to replace the last dash with the HTLM format of a dash (%2F)
-  # Only if there is a dash in the color column
-  mutate(url_color = ifelse(str_detect(colors, pattern = "/"),
-                            
-                            # if TRUE --> replace      
-                            stringi::stri_replace_last_fixed(url_color, "/", "%2F"),
-                            
-                            # ELSE --> take the original url
-                            url_color))
-
-bike_data_colors_tbl %>% glimpse()
-
-
-
-
-# Create function
-get_sizes <- function(url) {
-  
-  json <- url %>%
-    
-    read_html() %>%
-    
-    # Get all 'script nodes' and convert to char
-    html_nodes(css = "script") %>%
-    as.character() %>%
-    
-    # Select the node, that contains 'window.deptsfra'
-    str_subset(pattern = "window.deptsfra") %>%
-    
-    # remove the chars that do not belong to the json
-    # 1. replace at the beginning everything until the first "{" with ""
-    str_replace("^[^\\{]+", "") %>%
-    # 2. replace at the end everything after the last "}" with ""
-    str_replace("[^\\}]+$", "") %>%
-    
-    # Convert from json to an r object and pick the relevant values
-    fromJSON(flatten = T) %>%
-    purrr::pluck("productDetail", "variationAttributes", "values", 2) %>%
-    
-    # select(id, value, available, availability)# %>%
-    select(id, value, availability.onlyXLeftNumber) %>%
-    
-    # Rename
-    rename(id_size = id) %>%
-    rename(size = value) %>%
-    rename(stock_availability = availability.onlyXLeftNumber) %>%
-    
-    # Conver to tibble
-    as_tibble()
-  
-}
-
-# Pull url vector
-bike_url_color_vec <- bike_data_colors_tbl %>% 
-  pull(url_color)
-
-# Map
-bike_data_sizes_tbl <- bike_data_colors_tbl %>% 
-  mutate(size = future_map(bike_url_color_vec, get_sizes))
-
-# Unnest
-bike_data_sizes_tbl <- bike_data_sizes_tbl %>% 
-  unnest(size)
-
-saveRDS(bike_data_sizes_tbl, "Data_acquisition/bike_data_sizes_tbl.rds")
 
